c908c68fbb7a56f0f8a0ca9709d204de17182dcf
Дмитро <119788810+Raidentheripper@users.noreply.github.com>
2025-11-20 12:20:47 +0200

    Add fetchUsers function to fetch user data
    Implemented fetchUsers function to retrieve user data from the API and return it in an array format.

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,13 +1,28 @@
 "Ваша функція повинна робити GET-запит до вказаного URL і отримати дані."
 "Поверніть дані користувачів у форматі масиву"
 "Дані мають включати такі поля, як id та name."
-
-"https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
-
 function fetchUsers() {
-  // Ваш код
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}, ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      return data.map(user => ({
+        id: user.id,
+        name: user.name
+      }));
+    })
+    .catch(error => {
+      return new Error(`Error: ${error.message}`);
+    });
 }
 
-console.log(fetchUsers())
+fetchUsers().then(result => console.log(result));
 
+// Експортуємо функцію
 module.exports = fetchUsers;
